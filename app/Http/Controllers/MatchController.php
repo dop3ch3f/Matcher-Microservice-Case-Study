6903a3896aeb5eff6a58ac3e6541ae7c9844c31e
ifeanyi ibekie <ifeanyi.ibekie@gmail.com>
2021-10-16 19:10:13 +0100

    added comments and cleaned up the process

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\PropertyResource;
-use App\Http\Resources\SearchProfileResource;
 use App\Models\Property;
 use App\Models\SearchProfile;
-use Illuminate\Http\Request;
 
 
 class MatchController extends Controller
 {
-    public function computeMatchScore(Property $property, SearchProfile $searchProfile) {
-        return 0;
-    }
 
     public function getDeviatedNumber($num, $increment = false): int {
         if($increment) {
@@ -23,12 +17,14 @@ class MatchController extends Controller
     }
 
     public function computeLooseMatch($propertyValue, $searchProfileValue) : bool {
-        // for direct field type
+        // if not array return false
         if (!is_array($searchProfileValue)) {
             return false;
         }
+        // get the range values from array
         [$min, $max] = $searchProfileValue;
 
+        // if null set it to pass because it is actually a pass in the conditions
         $min = is_null($min) ? 'pass' : (int) $min;
         $max = is_null($max) ? 'pass' : (int) $max;
 
@@ -56,6 +52,7 @@ class MatchController extends Controller
 
         $propertyFields = json_decode($property->fields, true);
 
+        // for each searchProfile fields compute the strict match and loose match values
         foreach (json_decode($searchProfile->search_fields, true) as $field => $value) {
                 if(isset($propertyFields[$field])) {
                     if($this->computeStrictMatch($propertyFields[$field], $value)) {
@@ -68,7 +65,9 @@ class MatchController extends Controller
                 }
         }
 
+        // return result
         return [
+            // the value of strict match is doubled to give it more weight that loose match in comparison with other results
             'score' => (2 * $strict_match_count) + ($loose_match_count),
             'strictMatchesCount' => $strict_match_count,
             'looseMatchesCount' => $loose_match_count,
@@ -77,9 +76,11 @@ class MatchController extends Controller
 
     public function getPropertyMatches(Property $property) {
 
+        // initialize query builder
         $query = SearchProfile::query();
 
         foreach (json_decode($property->fields, true) as $key => $value ) {
+            // for each property field create a query entry on the searchprofile query instance
             if (isset($value)) {
                 if($key === 'returnActual') {
                     $query = $query->orWhereJsonContains('return_potential', [$value]);
@@ -89,18 +90,19 @@ class MatchController extends Controller
             }
         }
 
-
+        // convert the query result into the expected api response structure
         $result = array_map(function ($searchProfile) use ($property) {
+            // compute the match information between the property and a single search profile
             $result = $this->computeMatches($property, $searchProfile);
             return [
                 'searchProfileId' => $searchProfile->id,
-                'searchProfile' => new SearchProfileResource($searchProfile),
                 'score' => $result['score'],
                 'strictMatchesCount' => $result['strictMatchesCount'],
                 'looseMatchesCount' => $result['looseMatchesCount'],
             ];
         }, $query->get()->all());
 
+        // sort the result by order of score
         usort($result, function ($a, $b) {
             if ($a['score'] == $b['score']) {
                 return 0;
